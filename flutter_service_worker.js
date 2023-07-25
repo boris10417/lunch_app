a262b6d2f923c1843e828f5bb45c0ea6ed2824fc
boris <boris10417@gmail.com>
2023-07-25 15:12:21 +0800

    修改:
    1.將儲存、讀取、刪除封裝到選單中
    
    2.動態調整的輸入框，可以隨著可用範圍變形了

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "d58b3210ce42b4939620ba7b9cf90314",
+"assets/fonts/MaterialIcons-Regular.otf": "d11b42ee6de50e638aeae6f8c7392eb1",
 "assets/NOTICES": "002163f00e6f1bede2ca6a13e1a687a3",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "428075c960d8a583b2301a91f22e2287",
-"/": "428075c960d8a583b2301a91f22e2287",
-"main.dart.js": "f83c83f24f64cc27945aa0b5032b5a9e",
+"index.html": "70eed240d893989b17bd1ca87f3d5506",
+"/": "70eed240d893989b17bd1ca87f3d5506",
+"main.dart.js": "7999acd7daa621ebb5560bfa83bebeee",
 "manifest.json": "3f9f46d43b9a25da68f5dd4800d5ed6d",
 "version.json": "c2a08cda2a1e690bfed5ce9fc4d9e5b1"};
 // The application shell files that are downloaded before a service worker can
