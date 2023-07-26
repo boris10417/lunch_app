8dedc129f1898cf73a002128339790e361159353
boris <boris10417@gmail.com>
2023-07-26 17:06:13 +0800

    新增:網頁版的匯入匯出功能，手機瀏覽器需要測試後才知道是否能夠使用，電腦版僅測試過windows

--- develope/flutter_service_worker.js
+++ develope/flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "d11b42ee6de50e638aeae6f8c7392eb1",
+"assets/fonts/MaterialIcons-Regular.otf": "a3665879d71cc8ec9d40a1d32b4e3ffe",
 "assets/NOTICES": "002163f00e6f1bede2ca6a13e1a687a3",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "48ed550c8daac34992477988d3f23f03",
-"/": "48ed550c8daac34992477988d3f23f03",
-"main.dart.js": "da5150acb23221944238c594dc996ee0",
+"index.html": "125d3069c93ad978521c76bca460bbbe",
+"/": "125d3069c93ad978521c76bca460bbbe",
+"main.dart.js": "3f4a931d4a2fe77a336952ac26dcd343",
 "manifest.json": "3f9f46d43b9a25da68f5dd4800d5ed6d",
 "version.json": "c2a08cda2a1e690bfed5ce9fc4d9e5b1"};
 // The application shell files that are downloaded before a service worker can
